47f678f90e0dfe807c1bf61b65a024a861229fb8
CatherineProulx <48301264+CatherineProulx@users.noreply.github.com>
2026-01-17 02:42:02 +0000

    Create endpoints and FamilyStructure data structure

--- src/app.py
+++ src/app.py
@@ -30,13 +30,56 @@ def sitemap():
 
 
 @app.route('/members', methods=['GET'])
-def handle_hello():
+def get_family():
     # This is how you can use the Family datastructure by calling its methods
     members = jackson_family.get_all_members()
-    response_body = {"hello": "world",
-                     "family": members}
-    return jsonify(response_body), 200
+    return jsonify(members), 200
 
+
+@app.route('/members/<int:id>', methods=['GET'])
+def get_member(id):
+    member = jackson_family.get_member(int(id))
+    return jsonify(member[0]), 200
+
+
+@app.route('/members', methods=['POST'])
+def add_member():
+    new_member = request.get_json(force=True)
+
+    # Validate user input
+    if not new_member:
+        return jsonify({"error": "Request body is required"}), 400
+
+    required_fields = {'first_name', 'age', 'lucky_numbers'}
+    for field in required_fields:
+        if field not in new_member:
+            return jsonify({"error": f"Missing field: {field}"}), 400
+
+    for key in new_member:
+        if key not in required_fields:
+            return jsonify({"error": f"Extra field: {key}"}), 400
+
+    if not isinstance(new_member["first_name"], str):
+        return jsonify({"error": "first_name must be a string"}), 400
+
+    if not isinstance(new_member["age"], int):
+        return jsonify({"error": "age must be an integer"}), 400
+
+    if not isinstance(new_member["lucky_numbers"], list):
+        return jsonify({"error": "lucky_numbers must be a List"}), 400
+
+    if not all(isinstance(n, int) for n in new_member["lucky_numbers"]):
+        return jsonify({"error": "lucky_numbers must contain only integers"}), 400
+
+    # Add new member
+    jackson_family.add_member(new_member)
+    return jsonify(new_member), 200
+
+
+@app.route('/members/<int:id>', methods=['DELETE'])
+def delete_member(id):
+    jackson_family.delete_member(int(id))
+    return jsonify({'done': True}), 200
 
 
 # This only runs if `$ python src/app.py` is executed
